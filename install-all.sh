f94761239b85fb974390aa80ce1a3f74599190a3
newgithub10 <78011427+newgithub10@users.noreply.github.com>
2021-02-12 09:22:21 +0530

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -8,7 +8,7 @@ sudo service nginx stop
 #Java
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt update
-sudo apt install oracle-java8-installer -y
+#sudo apt install oracle-java8-installer -y
 
 #Git and Maven
 sudo apt update
